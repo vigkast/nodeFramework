d20db5293d5b01123da1fbe64f2a24b0258560c4
Chintan <chintan@wohlig.com>
2016-09-15 14:13:01 +0530

    accessToken

--- api/services/User.js
+++ api/services/User.js
@@ -4,6 +4,60 @@ var schema = new Schema({
         required: true,
         unique: true,
         uniqueCaseInsensitive: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        uniqueCaseInsensitive: true
+    },
+    photo: {
+        type: String,
+        required: true,
+        unique: true,
+        uniqueCaseInsensitive: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+        uniqueCaseInsensitive: true
+    },
+    forgotPassword: {
+        type: String,
+        required: true,
+        unique: true,
+        uniqueCaseInsensitive: true
+    },
+    otp: {
+        type: String,
+        required: true,
+        unique: true,
+        uniqueCaseInsensitive: true
+    },
+    accessToken: {
+        type: String,
+        required: true,
+        unique: true,
+        uniqueCaseInsensitive: true
+    },
+    googleID: {
+        type: String,
+        required: true,
+        unique: true,
+        uniqueCaseInsensitive: true
+    },
+    twitterID: {
+        type: String,
+        required: true,
+        unique: true,
+        uniqueCaseInsensitive: true
+    },
+    facebookID: {
+        type: String,
+        required: true,
+        unique: true,
+        uniqueCaseInsensitive: true
     }
 });
 
@@ -13,5 +67,9 @@ schema.plugin(timestamps);
 module.exports = mongoose.model('User', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
-var model = {};
+var model = {
+
+
+
+};
 module.exports = _.assign(module.exports, exports, model);
