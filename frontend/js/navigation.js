6b3f5821e380e9ceee9241e3489aec9a6ac95c48
Chintan <chintan@wohlig.com>
2016-10-04 13:32:38 +0530

    chgangs

--- frontend/js/navigation.js
+++ frontend/js/navigation.js
@@ -1,13 +1,6 @@
-var adminURL = "";
-if (isproduction) {
-    adminURL = "http://www.wohlig.co.in/demo/index.php";
-} else {
-    adminURL = "http://localhost/demo/index.php";
-}
-
 var navigationservice = angular.module('navigationservice', [])
 
-.factory('NavigationService', function() {
+.factory('NavigationService', function () {
     var navigation = [{
         name: "Home",
         classis: "active",
@@ -25,10 +18,10 @@ var navigationservice = angular.module('navigationservice', [])
     }];
 
     return {
-        getnav: function() {
+        getnav: function () {
             return navigation;
         },
-        makeactive: function(menuname) {
+        makeactive: function (menuname) {
             for (var i = 0; i < navigation.length; i++) {
                 if (navigation[i].name == menuname) {
                     navigation[i].classis = "active";
