f7d0ed823a89582abc9a957ec0442a5db1ee9eff
System Administrator <root@Rajs-MacBook-Pro.local>
2016-10-11 17:24:33 +0530

    chnages for the same

--- backend/js/controllers.js
+++ backend/js/controllers.js
@@ -80,8 +80,10 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
 .controller('DetailCtrl', function ($scope, TemplateService, NavigationService, JsonService, $timeout, $state, $stateParams) {
     $scope.json = JsonService;
     $scope.template = TemplateService;
+    $scope.data = {
+        email: "Chinatn"
+    };
 })
-
 
 
 .controller('LoginCtrl', function ($scope, TemplateService, NavigationService, $timeout, $stateParams, $state) {
