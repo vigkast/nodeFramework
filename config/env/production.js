0b6acf58c5ffe2615401bc802e75fe980161cc53
Chintan <chintan@wohlig.com>
2016-12-27 12:08:18 +0530

    Revert "yes"
    This reverts commit bb1dfe48a2197625496e9922c21280818e68bf00.

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 95,
-  realHost: "https://nodeframework.wohlig.com",
+  realHost: "http://nodeframework.wohlig.com",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 
   /***************************************************************************
