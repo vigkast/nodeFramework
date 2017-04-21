d68281b385b053d65573efe03c7b0c7f4f84f4b5
Chintan <chintan@wohlig.com>
2017-04-21 16:56:50 +0530

    ok

--- tasks/config/browserSync.js
+++ tasks/config/browserSync.js
@@ -1,6 +1,10 @@
 var historyApiFallback = require('connect-history-api-fallback');
 module.exports = function (grunt) {
-    var folderName = grunt.option('target');
+    var folderName = grunt.option('target') || "frontend";
+    var route = {
+        "/bower_components": "bower_components"
+    };
+    route["/" + folderName] = folderName;
     grunt.config.set('browserSync', {
         bsFiles: {
             src: [folderName + '/**/*.css', folderName + "/**/*.js", folderName + "/**/*.html", folderName + "/img/**"]
@@ -8,9 +12,7 @@ module.exports = function (grunt) {
         options: {
             server: {
                 baseDir: folderName,
-                routes: {
-                    "/bower_components": "bower_components"
-                },
+                routes: route,
                 middleware: [historyApiFallback()]
             },
             port: 8080,
