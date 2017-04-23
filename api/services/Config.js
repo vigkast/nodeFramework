8daa3aefdbde10d197281f12ab01a1bfc957218d
Chintan <chintan@wohlig.com>
2017-04-23 15:04:07 +0530

    email setting done as well

--- api/services/Config.js
+++ api/services/Config.js
@@ -358,6 +358,27 @@ var models = {
             fs.unlink(dest);
             callback(err);
         });
+    },
+    sendEmail: function (fromEmail, toEmail, subject, filename, data) {
+        var helper = require('sendgrid').mail;
+        var from_email = new helper.Email(fromEmail);
+        var to_email = new helper.Email(toEmail);
+        sails.hooks.views.render("email/" + filename, data, function (err, html) {
+            var content = new helper.Content('text/html', html);
+            var mail = new helper.Mail(from_email, subject, to_email, content);
+            var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+            var request = sg.emptyRequest({
+                method: 'POST',
+                path: '/v3/mail/send',
+                body: mail.toJSON(),
+            });
+            sg.API(request, function (error, response) {
+                console.log(response.statusCode);
+                console.log(response.body);
+                console.log(response.headers);
+            });
+        });
+
     }
 };
 module.exports = _.assign(module.exports, models);
