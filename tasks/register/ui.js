671e2a496ca020309485135c9c871ba241a8cb2f
Chintan <chintan@wohlig.com>
2017-06-09 15:00:20 +0530

    done

--- tasks/register/ui.js
+++ tasks/register/ui.js
@@ -2,7 +2,7 @@ module.exports = function (grunt) {
     var folderName = grunt.option('target');
     var isProduction = grunt.option('production');
     if (isProduction) {
-        grunt.registerTask('ui', ["ejs:ui", "sass:development", "cssmin:production", "concat:production", "uglify"]);
+        grunt.registerTask('ui', ["ejs:ui", "sass:development", "cssmin:production", "concat:production", "uglify", "prod"]);
     } else {
         grunt.registerTask('ui', ["ejs:ui", "sass:development", "concurrent:watchDevelopment"]);
     }
